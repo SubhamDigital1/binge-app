f0fa547acd1e968bf4b16f100e0d0fddd74fe24b
SubhamDigital1 <subhamparihari1@gmail.com>
2026-05-14 20:22:10 +0530

    Refactor service worker to use Workbox caching
    Updated service worker to use Workbox for caching and added message listener for skip waiting.

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,18 @@
-const CACHE_NAME = 'dropott-cache-v2';
+// This is the "Offline copy of pages" service worker
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+const CACHE = "pwabuilder-offline";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
